5f6b2a26307284753ad563c97e90430ee13918aa
troopdegen <mel@innvertir.com>
2025-04-01 10:18:00 -0600

    add pulpa token contract (optimism)

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,21 +1,20 @@
 import { createConfig } from "ponder";
 import { http } from "viem";
-import { erc20ABI } from "./abis/erc20ABI";
+import { pulpaTokenABI } from "./abis/PulpaTokenABI";
 
 export default createConfig({
   networks: {
-    mainnet: {
-      chainId: 1,
-      transport: http(process.env.PONDER_RPC_URL_1),
+    optimism: {
+      chainId: 10,
+      transport: http(process.env.PONDER_RPC_URL_10),
     },
   },
   contracts: {
-    ERC20: {
-      network: "mainnet",
-      abi: erc20ABI,
-      address: "0x32353A6C91143bfd6C7d363B546e62a9A2489A20",
-      startBlock: 13142655,
-      endBlock: 13150000,
+    PulpaToken: {
+      network: "optimism",
+      abi: pulpaTokenABI,
+      address: process.env.PULPA_OPTIMISM_ADDRESS as `0x${string}`,
+      startBlock: 115537645,
     },
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -6,46 +6,46 @@ import {
   transferEvent,
 } from "ponder:schema";
 
-ponder.on("ERC20:Transfer", async ({ event, context }) => {
+ponder.on("PulpaToken:Transfer", async ({ event, context }) => {
   await context.db
     .insert(account)
     .values({ address: event.args.from, balance: 0n, isOwner: false })
     .onConflictDoUpdate((row) => ({
-      balance: row.balance - event.args.amount,
+      balance: row.balance - event.args.value,
     }));
 
   await context.db
     .insert(account)
     .values({ address: event.args.to, balance: 0n, isOwner: false })
     .onConflictDoUpdate((row) => ({
-      balance: row.balance + event.args.amount,
+      balance: row.balance + event.args.value,
     }));
 
   // add row to "transfer_event".
   await context.db.insert(transferEvent).values({
     id: event.id,
-    amount: event.args.amount,
+    amount: event.args.value,
     timestamp: Number(event.block.timestamp),
     from: event.args.from,
     to: event.args.to,
   });
 });
 
-ponder.on("ERC20:Approval", async ({ event, context }) => {
+ponder.on("PulpaToken:Approval", async ({ event, context }) => {
   // upsert "allowance".
   await context.db
     .insert(allowance)
     .values({
       spender: event.args.spender,
       owner: event.args.owner,
-      amount: event.args.amount,
+      amount: event.args.value,
     })
-    .onConflictDoUpdate({ amount: event.args.amount });
+    .onConflictDoUpdate({ amount: event.args.value });
 
   // add row to "approval_event".
   await context.db.insert(approvalEvent).values({
     id: event.id,
-    amount: event.args.amount,
+    amount: event.args.value,
     timestamp: Number(event.block.timestamp),
     owner: event.args.owner,
     spender: event.args.spender,
