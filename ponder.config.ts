f047d3682868d986452fe5b20c7c41ad76447937
troopdegen <mel@innvertir.com>
2025-04-01 11:04:20 -0600

    add xoc token contracts (polygon + base)

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,12 +1,21 @@
 import { createConfig } from "ponder";
 import { http } from "viem";
 import { pulpaTokenABI } from "./abis/PulpaTokenABI";
+import { xocolatlTokenABI } from "./abis/XocolatlTokenABI";
 
 export default createConfig({
   networks: {
+    base: {
+      chainId: 8453,
+      transport: http(process.env.PONDER_RPC_URL_8453),
+    },
     optimism: {
       chainId: 10,
       transport: http(process.env.PONDER_RPC_URL_10),
+    },
+    polygon: {
+      chainId: 137,
+      transport: http(process.env.PONDER_RPC_URL_137),
     },
   },
   contracts: {
@@ -15,6 +24,19 @@ export default createConfig({
       abi: pulpaTokenABI,
       address: process.env.PULPA_OPTIMISM_ADDRESS as `0x${string}`,
       startBlock: 115537645,
+    },
+    XocToken: {
+      abi: xocolatlTokenABI,
+      network: {
+        base: {
+          address: process.env.XOC_BASE_ADDRESS as `0x${string}`,
+          startBlock: 2338635,
+        },
+        polygon: {
+          address: process.env.XOC_POLYGON_ADDRESS as `0x${string}`,
+          startBlock: 32141175,
+        },
+      },
     },
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -51,3 +51,28 @@ ponder.on("PulpaToken:Approval", async ({ event, context }) => {
     spender: event.args.spender,
   });
 });
+
+ponder.on("XocToken:Transfer", async ({ event, context }) => {
+  await context.db
+    .insert(account)
+    .values({ address: event.args.from, balance: 0n, isOwner: false })
+    .onConflictDoUpdate((row) => ({
+      balance: row.balance - event.args.value,
+    }));
+
+  await context.db
+    .insert(account)
+    .values({ address: event.args.to, balance: 0n, isOwner: false })
+    .onConflictDoUpdate((row) => ({
+      balance: row.balance + event.args.value,
+    }));
+
+  // add row to "transfer_event".
+  await context.db.insert(transferEvent).values({
+    id: event.id,
+    amount: event.args.value,
+    timestamp: Number(event.block.timestamp),
+    from: event.args.from,
+    to: event.args.to,
+  });
+});
